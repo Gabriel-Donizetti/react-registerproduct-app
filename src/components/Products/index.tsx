1675d20b45338f50da4211f5fbab33644ab9b8ee
Rodrigo Kovalski <rodrigokvk@gmail.com>
2021-11-04 01:53:42 -0200

    Adicionando estrutura de produtos

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -4,6 +4,47 @@ export function Products(){
     return(
         <div className={style.products}>
             <h1>Roupas</h1>
+
+            <div>
+                <table className="table table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Quantidade</th>
+                            <th scope="col">Preço</th>
+                            <th scope="col">Descrição</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Roupa</td>
+                            <td>1</td>
+                            <td>R$ 20,00</td>
+                            <td>Descrição</td>
+                            <td><i className="fas fa-edit">&nbsp;<i className="fas fa-trash"></i> </i></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Roupa</td>
+                            <td>15</td>
+                            <td>R$ 20,00</td>
+                            <td>Descrição</td>
+                            <td><i className="fas fa-edit">&nbsp;<i className="fas fa-trash"></i> </i></td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                            <td>Roupa</td>
+                            <td>2</td>
+                            <td>R$ 20,00</td>
+                            <td>Descrição</td>
+                            <td><i className="fas fa-edit">&nbsp;<i className="fas fa-trash"></i> </i></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
